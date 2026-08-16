a4f7644e05b954a8ee5d4c955cf51c4e7bd11de4
alicelucas14 <alicelucas025@gmail.com>
2026-08-16 06:35:00 +0100

    Fix preg_replace delimiter syntax error in rotator_norm_host

--- public/rotator_lib.php
+++ public/rotator_lib.php
@@ -39,9 +39,9 @@ function rotator_save($data) {
 
 function rotator_norm_host($h) {
     $h = strtolower(trim((string)$h));
-    $h = preg_replace('#^https?://#i', '', $h); // strip protocol scheme if present
-    $h = preg_replace('#[/\\\\?#].*+$#', '', $h); // strip path / query / trailing slash
-    $h = preg_replace('/:\d+$/', '', $h); // strip port
+    $h = preg_replace('~^https?://~i', '', $h); // strip protocol scheme if present
+    $h = preg_replace('~[/?#].*+$~', '', $h);   // strip path / query / trailing slash
+    $h = preg_replace('~:\d+$~', '', $h);      // strip port
     return $h;
 }
 
